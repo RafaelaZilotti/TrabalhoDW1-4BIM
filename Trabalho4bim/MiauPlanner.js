e77aa37fd797cc6bf26a5bb565fb6eeea312f2a9
RafaelaZilotti <164359029+RafaelaZilotti@users.noreply.github.com>
2024-11-21 11:58:15 -0300

    MiauPlannerBase

---
+++ Trabalho4bim/MiauPlanner.js
@@ -0,0 +1,25 @@
+const dark = document.querySelector("#darkmode")
+const fecha = document.querySelector("#fecha")
+const sidebar = document.querySelector("#sidebar")
+//add const p mudar cor
+
+.addEventListener("ctrl + h",mostraSidebar)
+fecha.addEventListener("click",fechaSidebar)
+dark.addEventListener("click", modoDark)
+
+
+function mostraSidebar(){
+    sidebar.style.right = "0"
+}
+
+function fechaSidebar(){
+    sidebar.style.right = "-100%"
+}
+
+function modoDark{
+    if(){
+        //ver c ta no modo dark, se nao
+    } else {
+
+    }
+}
